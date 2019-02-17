c725f7282ac1a4f0d69b0db55f950b7439c707c6
justmine <justmine@DESKTOP-UILU6SC>
2019-02-17 22:44:39 +0800

    第一次添加代码

---
+++ MultiLanguage/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace MultiLanguage.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static List<CultureInfo> GetCultures(this IConfiguration configuration, string key = "SupportedCultures")
+        {
+            var section = configuration.GetSection(key);
+            var cultures = new List<CultureInfo>();
+            foreach (var culture in section.AsEnumerable())
+            {
+                if (!string.IsNullOrEmpty(culture.Value))
+                {
+                    cultures.Add(new CultureInfo(culture.Value));
+                }
+            }
+
+            return cultures;
+        }
+    }
+
+}
---
+++ MultiLanguage/Extensions/LocalizationMiddlewareExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+
+namespace MultiLanguage.Extensions
+{
+    public static class LocalizationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLocalization(
+            this IApplicationBuilder app,
+            IConfiguration configuration)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+
+            var supportedCultures = configuration.GetCultures();
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(CultureInfo.CurrentCulture),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            return app;
+        }
+    }
+
+}
