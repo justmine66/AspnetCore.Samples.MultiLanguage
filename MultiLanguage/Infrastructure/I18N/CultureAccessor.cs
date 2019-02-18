4b09d58b2f2b40c2bb21b6d2c14db03c99f3404d
justmine <justmine@DESKTOP-UILU6SC>
2019-02-18 22:39:35 +0800

    1

---
+++ MultiLanguage/Infrastructure/I18N/CultureAccessor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MultiLanguage.Infrastructure.I18N
+{
+    public class CultureAccessor : ICultureAccessor
+    {
+        private readonly RequestLocalizationOptions _options;
+        private readonly IHttpContextAccessor _accessor;
+        public CultureAccessor(
+            IHttpContextAccessor _accessor,
+            IOptions<RequestLocalizationOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public IList<CultureInfo> SupportedCultures => _options.SupportedCultures;
+
+        public IList<CultureInfo> SupportedUICultures => _options.SupportedUICultures;
+
+        public RequestCulture RequestCulture => _accessor.HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture;
+    }
+}
---
+++ MultiLanguage/Infrastructure/I18N/ICultureAccessor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MultiLanguage.Infrastructure.I18N
+{
+    /// <summary>
+    /// 区域性访问器
+    /// </summary>
+    public interface ICultureAccessor
+    {
+        IList<CultureInfo> SupportedCultures { get; }
+        IList<CultureInfo> SupportedUICultures { get; }
+        RequestCulture RequestCulture { get; set; }
+    }
+}
